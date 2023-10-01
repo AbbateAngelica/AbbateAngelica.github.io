7309ce9f36ac81bb47126a006c8dbbd65c4bd9e2
Angelica <angelicabbate@gmail.com>
2023-09-30 23:22:50 -0300

    crear javascript y agregar clipboard.js

---
+++ main.js
@@ -0,0 +1,9 @@
+var clipboard = new ClipboardJS('.js-mail-copy');
+
+clipboard.on('success', function(e) {
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+
+    e.clearSelection();
+});
