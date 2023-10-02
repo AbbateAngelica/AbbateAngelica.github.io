366fa3eae45ba39198df3317235c2e592e45eebc
Angelica <angelicabbate@gmail.com>
2023-10-01 22:10:51 -0300

    Agregar cuadro de texto a la accion de copiar

--- main.js
+++ main.js
@@ -5,5 +5,16 @@ clipboard.on('success', function(e) {
     console.info('Text:', e.text);
     console.info('Trigger:', e.trigger);
 
+    document.getElementById('notificacion').style.display = "block";
+
+    setTimeout(function() {
+      notificacion.style.display = "none";
+    }, 2000);
+
     e.clearSelection();
+});
+
+clipboard.on('error', function(e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
 });
